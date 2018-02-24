c1a3e5d0774007286f52a9f137a06a24681bdafa
Carter Konz <carter.konz@gmail.com>
2018-02-23 18:25:53 -0600

    Update epic-games.rb

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -1,9 +1,7 @@
 cask 'epic-games' do
-  version '2.12.24-3321064'
-  sha256 'b1e37a5ff042fb5e13cd904d71acb14496bd65036612a726ff3b509947779c1d'
 
   # epicgames.com was verified as official when first introduced to the cask
-  url "https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicGamesLauncher-#{version}.dmg"
+  url "https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncher.dmg"
   name 'Epic Games Launcher'
   homepage 'https://www.unrealengine.com/what-is-unreal-engine-4'
 
